9de078d1df637ac3f97e55d54d02a6bdaf6c043b
jandoso <carol.jandoso@gmail.com>
2019-10-25 22:08:51 -0300

    adição de rotas extras

--- backend/routes.js
+++ backend/routes.js
@@ -7,7 +7,11 @@ router.get('/livros', livrosController.get);
 router.get('/livros/:id', livrosController.getBookById);
 router.get('/livros/autores', livrosController.getAuthors);
 router.get('/livros/generos', livrosController.getGenders);
-
+router.get('/autores/ativos', livrosController.getActiveAuthors);
+router.get('/autores/:ano/nascimento', livrosController.getAuthorsByDateOfBirth);
+router.get('/autores/:id/livros', livrosController.getBooksByAuthor);
+router.get('/autores/:id/ativos/livros', livrosController.getBooksFromActiveAuthor);
+router.get('/generos/buscar/:nome/livros', livrosController.getBooksByGender);
 
 
 module.exports = router;
--- backend/src/controllers/livrosController.js
+++ backend/src/controllers/livrosController.js
@@ -1,4 +1,6 @@
 const livros = require('../models/livros.json');
+const autores = require('../models/autores.json');
+const generos = require('../models/generos.json');
 
 exports.get = (req, res) => {
     res.status(200).send(livros);
@@ -18,4 +20,39 @@ exports.getAuthors = (req, res) => {
 exports.getGenders = (req, res) => {
     const generos = livros.map(livro => livro.genero);
     res.status(200).send(generos);
+};
+
+exports.getBooksByAuthor = (req, res) => {
+    const autorId = req.params.id;
+    const nomeAutor = autores.find(autor => autor.idAutor == autorId);
+    const livrosAutor = livros.filter(livro => livro.autor == nomeAutor.nome);
+    res.status(200).send(livrosAutor);
+};
+
+exports.getBooksByGender = (req, res) => {
+    const genero = req.params.nome;
+    const livrosGenero = livros.filter(livro => livro.genero.indexOf(genero) > -1);
+    res.status(200).send(livrosGenero);
+};
+
+exports.getActiveAuthors = (req, res) => {
+    const autoresAtivos = autores.filter(autor => autor.ativo == true);
+    res.status(200).send(autoresAtivos);
+};
+
+exports.getBooksFromActiveAuthor = (req, res) => {
+    const autorId = req.params.id;
+    const nomeAutor = autores.find(autor => autor.idAutor == autorId);
+    if(nomeAutor.ativo == true){
+        const livrosAutorAtivo = livros.filter(livro => livro.autor == nomeAutor.nome);
+        res.status(200).send(livrosAutorAtivo);
+    }else{
+        res.status(404).send("Autor não ativo");
+    }; 
+};
+
+exports.getAuthorsByDateOfBirth = (req, res) => {
+    const ano = req.params.ano;
+    const autoresMaisNovos = autores.filter(autor => autor.dataNascimento > ano);
+    res.status(200).send(autoresMaisNovos);
 };
